fe513649cd17e6d290baadb3067eaa83fd0e31be
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2021-12-27 12:26:17 +0500

    Transaction Cards With Styling.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expense_planner/transactions.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,6 +19,12 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transactions> transactions = [
+    Transactions(
+        id: "t1", title: "New Shoes", amount: 1500, date: DateTime.now()),
+    Transactions(id: "t2", title: "Jacket", amount: 3000, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,13 +34,66 @@ class MyHomePage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: const [
-          Card(
+        children: [
+          const Card(
             child: Text("Card 1"),
             elevation: 5,
           ),
-          Card(
-            child: Text("Card 2"),
+          Column(
+            children: transactions.map((e) {
+              return Card(
+                elevation: 5,
+                child: Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(21, 10, 15, 10),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 1,
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 11,
+                        vertical: 13,
+                      ),
+                      child: Text(
+                        e.amount.toString(),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 3),
+                      height: 43,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            e.title,
+                            style: const TextStyle(
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            e.date.toString(),
+                            style: const TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
           ),
         ],
       ),
