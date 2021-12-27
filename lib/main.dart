cb1d1c591688faa664a569e519fd3002faad3210
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2021-12-27 12:49:35 +0500

    Container around body for color, Scaffold color, app bar color

--- lib/main.dart
+++ lib/main.dart
@@ -28,74 +28,81 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFF6D7A7),
       appBar: AppBar(
+        backgroundColor: const Color(0xFF87AAAA),
         title: const Text("Expense Planner"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Card(
-            child: Text("Card 1"),
-            elevation: 5,
-          ),
-          Column(
-            children: transactions.map((e) {
-              return Card(
-                elevation: 5,
-                child: Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.fromLTRB(21, 10, 15, 10),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 1,
+      body: Container(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 5,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Card(
+              child: Text("Card 1"),
+              elevation: 5,
+            ),
+            Column(
+              children: transactions.map((e) {
+                return Card(
+                  elevation: 5,
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(17, 10, 15, 10),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 1,
+                          ),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 11,
+                          vertical: 13,
+                        ),
+                        child: Text(
+                          e.amount.toString(),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                          ),
                         ),
                       ),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 11,
-                        vertical: 13,
-                      ),
-                      child: Text(
-                        e.amount.toString(),
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 17,
+                      Container(
+                        margin: EdgeInsets.only(top: 3),
+                        height: 45,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              e.title,
+                              style: const TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              e.date.toString(),
+                              style: const TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 3),
-                      height: 43,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            e.title,
-                            style: const TextStyle(
-                              fontSize: 19,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            e.date.toString(),
-                            style: const TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
-        ],
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
